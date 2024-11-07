360aab2b62705fdc56ea4b54269bbc06367f4b24
Daimler <dvalverde@teledolar.net>
2024-11-07 14:18:47 -0600

    Add searchText balance

--- components/Balances/index.tsx
+++ components/Balances/index.tsx
@@ -1,19 +1,50 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text, View, TextInput } from "react-native";
 import {styled} from "@gluestack-style/react"
 import {Spinner} from "@gluestack-ui/themed"
 import useGetBalance from "@/hooks/useGetBalance";
 import { entityMaster } from "@/utils/constant";
 import CardBalance from "../CardBalance";
+import { useEffect, useState } from "react";
 
 const WalletMasterBalance = () => {
 	const { balance, loading, error } = useGetBalance(entityMaster);
+	const [searchText, setSearchText] = useState("");
+	const [filteredBalance, setFilteredBalance] = useState([]);
+
+	useEffect(() => {
+		  if (searchText) {
+			setFilteredBalance(
+			  balance.filter((item: any) =>
+				item.paymentMethodAlias
+				  .toLowerCase()
+				  .includes(searchText.toLowerCase())
+			  )
+			);
+		  } else {
+			setFilteredBalance(balance);
+		  }
+		
+	  }, [searchText, balance]);
+	  
 	return(
 		<Container>
 			{loading && <Spinner size={"small"} />}
 			{error && <Text>Error: {error.message}</Text>}
+			<TextInput
+				placeholder="Buscar por alias de método de pago"
+				value={searchText}
+				onChangeText={setSearchText}
+				style={{
+				padding: 10,
+				borderWidth: 1,
+				borderColor: "#ccc",
+				marginBottom: 10,
+				borderRadius: 8,
+				}}
+			/>
 			<FlatList
 				showsVerticalScrollIndicator={false}
-				data={balance}
+				data={filteredBalance}
 				renderItem={({ item }) => (
 					<CardBalance {...item} />
 				)}
